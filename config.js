b95cb3a09deb0f94b02d8954e8d11108f7633a6e
Sathsaragimhannnnnn <157110173+Sathsaragimhannnnnn@users.noreply.github.com>
2024-01-25 18:57:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94764978991', 'SATHSARA', true],
+  ['94764978991', 'SATHSARA', true], 
   [''] 
 ] //Number of owners
 
@@ -50,9 +50,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.botname = 'KING BABIYA'
+global.premium = 'false'
+global.packname = 'SATHSARA' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
