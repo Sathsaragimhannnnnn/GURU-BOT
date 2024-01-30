f2a20aa03548f84fe4503fa1665c43a73a0d5dbc
Sathsaragimhannnnnn <157110173+Sathsaragimhannnnnn@users.noreply.github.com>
2024-01-30 07:24:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,9 +14,9 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['94764978991','94764978991'] 
-global.prems = ['94764978991', '94764978991', '94764978991']
-global.allowed = ['94764978991', '94764978991', '94764978991']
+global.mods = ['94713263228','94764978991'] 
+global.prems = ['94713263228', '94764978991', '94785784980']
+global.allowed = ['94713263228', '94764978991', '94766242237']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
